a03780ca6f5d51f236c7c01d778b72413cb1f9f2
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-30 17:47:13 +0300

    Make class singleton & clean code

--- Ios App/Items/ItemsApiService.swift
+++ Ios App/Items/ItemsApiService.swift
@@ -8,33 +8,41 @@
 import Foundation
 
 final class ItemsApiService {
+    
+    private init() { }
+    static let shared = ItemsApiService()
+    
     private let url = URL(string: "http://127.0.0.1:8080/items")!
     private let session = URLSession.shared
     
-    func getItems( completionHandler: @escaping ([Item]?) -> Void){
+    func fetchItems(completionHandler: @escaping ([Item]?) -> Void){
         let task = session.dataTask(with: url){ (data, resp, err) in
-            if let jsonString = String(data: data!, encoding: .utf8) {
-                        print(jsonString)
-                     }
-            guard let data = data else {
+            
+            if let _ = err {
                 completionHandler(nil)
                 return
             }
             
-            if let items = try? JSONDecoder().decode([Item].self, from: data){
+            if let data = data, let items = try? JSONDecoder().decode([Item].self, from: data){
                 completionHandler(items)
             }
         }
+        
         task.resume()
     }
     
-    func add(item: Item, completionHandler: @escaping (Bool) -> Void){
+    func add(item: Item, errorHandler: @escaping (String?) -> Void){
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.httpBody = try! JSONEncoder().encode(item)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        let task = session.dataTask(with: request)
+        let task = session.dataTask(with: request){data, resp, err in
+            if err == nil {
+                errorHandler(nil)
+            }
+            
+        }
         task.resume()
     }
     
