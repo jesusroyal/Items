4c4d849cf85db04da10264cd3b0b003d5ddee35b
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-30 17:13:22 +0300

    Add color scheme

---
+++ Ios App/Items/Utils/ITColors.swift
@@ -0,0 +1,17 @@
+//
+//  ITColors.swift
+//  Items
+//
+//  Created by Mike Shevelinsky on 30.01.2021.
+//
+
+import UIKit
+
+enum Colors {
+    static let light = UIColor(red: 243  / 255.0, green: 232 / 255.0, blue: 238 / 255.0, alpha: 1.0)
+    static let lightGreen = UIColor(red: 186  / 255.0, green: 205 / 255.0, blue: 176 / 255.0, alpha: 1.0)
+    static let green = UIColor(red: 114  / 255.0, green: 155 / 255.0, blue: 121 / 255.0, alpha: 1.0)
+    static let blue = UIColor(red: 71  / 255.0, green: 91 / 255.0, blue: 99 / 255.0, alpha: 1.0)
+    static let black = UIColor(red: 46  / 255.0, green: 44 / 255.0, blue: 47 / 255.0, alpha: 1.0)
+
+}
