3760b7dd1838d7231d81d6cddc06878708a04f0e
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-30 17:47:28 +0300

    Clean code

--- Ios App/Items/ViewControllers/AddItemViewController.swift
+++ Ios App/Items/ViewControllers/AddItemViewController.swift
@@ -8,33 +8,82 @@
 import UIKit
 
 final class AddItemViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var location: UITextField!
-   
-    @IBAction func addItem(_ sender: UIButton) {
-        let name = self.name.text!
-        let location = self.location.text!
-        let item = Item(name: name, location: location, description: "test description")
-        ItemsApiService().add(item: item) { (arh) in
-            print(arh)
-        }
-    }
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var addButton: UIButton!
+    
+    // MARK: - Public Properties
+    
+    var addComplete: (() -> Void)?
+    
+    // MARK: - Private Properties
+    
+    private var isLoading = false
+    
+    // MARK: - Initializers
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Public Methods
+    // MARK: - Private Methods
+    
+    private func setupView(){
+        addButton.backgroundColor = Colors.green
+        addButton.layer.cornerRadius = addButton.frame.height / 2
     }
-    */
+    
+    private func getNewItem() -> Item? {
+        let name = self.name.text!
+        let location = self.location.text!
+        let discriptionString = self.descriptionText.text
+        return Item(name: name, location: location, description: discriptionString!)
+    }
+    
+    private func presentLoadingAlert() {
+        let alert = UIAlertController(title: nil, message: "Loading", preferredStyle: .alert)
+
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating()
+
+        alert.view.addSubview(loadingIndicator)
+        isLoading = true
+        present(alert, animated: true)
+    }
+    
+    private func dismissLoadingAlert() {
+        dismiss(animated: true) {
+            self.isLoading = false
+            self.dismiss(animated: true, completion: {
+                if self.addComplete != nil {
+                    self.addComplete!()
+                }
+            })
+        }
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func addItem(_ sender: UIButton) {
+        
+        if let item = getNewItem() {
+            presentLoadingAlert()
+            ItemsApiService.shared.add(item: item) { (arh) in
+                DispatchQueue.main.async {
+                    self.dismissLoadingAlert()
+                }
+            }
+        }
+    }
 
 }
--- Ios App/Items/ViewControllers/HomeTableViewController.swift
+++ Ios App/Items/ViewControllers/HomeTableViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 final class HomeTableViewController: UITableViewController {
     
     private let reuseIdentifier = "itemCell"
-    private let apiService = ItemsApiService()
     private var items = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        apiService.getItems { (items) in
-            guard let items = items else {return}
-            self.items = items
+        getItems()
+        self.title = "Items"
+    }
+    
+    private func getItems(){
+        ItemsApiService.shared.fetchItems { (items) in
+            guard let items = items else { return }
             DispatchQueue.main.async {
+                self.items = items
                 self.tableView.reloadData()
             }
         }
@@ -27,34 +31,37 @@ final class HomeTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return items.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-
         cell.textLabel?.text = items[indexPath.row].name
-
         return cell
     }
     
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            apiService.delete(item: items[indexPath.row]) { (aga) in
-                print(aga)
-            }
+            ItemsApiService.shared.delete(item: items[indexPath.row]) { (error) in }
             items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAdd" {
+            guard let destination = segue.destination as? AddItemViewController else { return }
+            destination.addComplete = { [weak self] in
+                self?.getItems()
+            }
+        }
+    }
 }
